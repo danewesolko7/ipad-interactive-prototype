769ebe745662517ff6d03e76913b433fe48fcaa4
Dane <danewesolko@gmail.com>
2018-04-07 19:14:30 -0400

    upload files

---
+++ assets/javascripts/main.js
@@ -0,0 +1,27 @@
+// Tabs
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tab");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tab-link");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Modal
+$(".modal-trigger").on("click", function() {
+  var modal = $(this).data("modal");
+  $(modal).show();
+});
+
+$(".modal").on("click", function(e) {
+  var className = e.target.className;
+  if(className === "modal" || className === "close"){
+    $(this).closest(".modal").hide();
+  }
+});
